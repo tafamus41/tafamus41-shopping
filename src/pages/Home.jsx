d2c9748a3d0a61f9d03c2fab815794309e03ba2b
asimov <seyranmustafa4@gmail.com>
2024-09-15 19:23:33 +0300

    p

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Container, Row } from "react-bootstrap";
+import ProductCard from "../components/ProductCard";
 
 const Home = () => {
-  return (
-    <div>
-        
-    </div>
-  )
-}
+  const [product, setProduct] = useState("");
+  const url = "https://fakestoreapi.com/products";
+// console.log(product);
+  const getProducts = async () => {
+    const res = await axios.get(url);
+    // console.log(res.data);
+    setProduct(res.data);
+  };
+  useEffect(() => {
+    getProducts();
+  }, []);
 
-export default Home
+  return (
+    <Container>
+      <Row>
+        {product.map((item) => (
+          <ProductCard key={item.id} {...item}/>
+        ))}
+      </Row>
+    </Container>
+  );
+};
+
+export default Home;

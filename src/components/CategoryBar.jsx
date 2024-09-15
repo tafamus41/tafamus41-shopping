d9c26ba82a04323902cd0ac7ea0b8cf21cb73601
asimov <seyranmustafa4@gmail.com>
2024-09-15 17:40:35 +0300

    p

--- src/components/CategoryBar.jsx
+++ src/components/CategoryBar.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { Container } from "react-bootstrap";
+import Form from "react-bootstrap/Form";
 
 const CategoryBar = () => {
   return (
-    <div>CategoryBar</div>
-  )
-}
+    <Form.Group className="mb-3">
+      <Container className="d-flex gap-2 justify-content-evenly mt-5">
+        <Form.Check type="checkbox" label="All" />
+        <Form.Check type="checkbox" label="Electronics" />
+        <Form.Check type="checkbox" label="Jewelry" />
+        <Form.Check type="checkbox" label="Men's Clothing" />
+        <Form.Check type="checkbox" label="Women's Clothing" />
+      </Container>
+    </Form.Group>
+  );
+};
 
-export default CategoryBar
+export default CategoryBar;

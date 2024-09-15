f4fc9d0794ef334999d0f30d3f24c4ba91bc0f00
asimov <seyranmustafa4@gmail.com>
2024-09-15 17:08:41 +0300

    p

--- src/components/MyNavbar.jsx
+++ src/components/MyNavbar.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import logo from "../assets/tafamus41.png";
+import { SlBasket } from "react-icons/sl";
+import Badge from "react-bootstrap/Badge";
+import Button from "react-bootstrap/Button";
 
 const MyNavbar = () => {
   return (
-    <div>MyNavbar</div>
-  )
-}
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#">
+          <img src={logo} alt="" width={"50px"} />
+        </Navbar.Brand>
+        <Button variant="primary">
+          <SlBasket /> <Badge bg="secondary"></Badge>
+        </Button>
+      </Container>
+    </Navbar>
+  );
+};
 
-export default MyNavbar
+export default MyNavbar;

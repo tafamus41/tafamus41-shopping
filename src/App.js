25da4360c5e9f542805797828d70d307ce2ab828
asimov <seyranmustafa4@gmail.com>
2024-09-15 17:19:03 +0300

    p

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import AppRouter from "./router/AppRouter";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
--- src/components/MyNavbar.jsx
+++ src/components/MyNavbar.jsx
@@ -1,4 +1,4 @@
-import Container from "react-bootstrap/Container";
+
 import Navbar from "react-bootstrap/Navbar";
 import logo from "../assets/tafamus41.png";
 import { SlBasket } from "react-icons/sl";
@@ -7,15 +7,15 @@ import Button from "react-bootstrap/Button";
 
 const MyNavbar = () => {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#">
-          <img src={logo} alt="" width={"50px"} />
+    <Navbar expand="lg" className="bg-body-tertiary d-flex justify-content-between align-items-center">
+      
+        <Navbar.Brand>
+          <img src={logo} alt="" width={"100px"} />TAFAMUS41 SHOPPING
         </Navbar.Brand>
-        <Button variant="primary">
+        <Button variant="primary" width={"100px"}  >
           <SlBasket /> <Badge bg="secondary"></Badge>
         </Button>
-      </Container>
+    
     </Navbar>
   );
 };

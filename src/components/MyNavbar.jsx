112d459f9947eda8fb7597bc26d3d7db9486544c
asimov <seyranmustafa4@gmail.com>
2024-09-18 05:19:25 +0300

    p

--- src/components/MyNavbar.jsx
+++ src/components/MyNavbar.jsx
@@ -8,14 +8,12 @@ import Button from "react-bootstrap/Button";
 const MyNavbar = ({counter,renderBasket}) => {
   return (
     <Navbar expand="lg" className="bg-body-tertiary d-flex justify-content-between align-items-center px-5">
-      
         <Navbar.Brand className="d-flex align-items-center gap-4" >
           <img src={logo} alt="" width={"75px"} /> <span>TAFAMUS41 SHOPPING</span> 
         </Navbar.Brand>
         <Button variant="primary" width={"100px"} onClick={renderBasket} >
           <SlBasket /> <Badge bg="secondary">{counter}</Badge>
         </Button>
-    
     </Navbar>
   );
 };

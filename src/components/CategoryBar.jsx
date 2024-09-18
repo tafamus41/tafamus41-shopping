06e82dd74063e7dfb1badc007caee2e3aaa9f7bf
asimov <seyranmustafa4@gmail.com>
2024-09-18 05:25:43 +0300

    p

--- src/components/CategoryBar.jsx
+++ src/components/CategoryBar.jsx
@@ -6,7 +6,7 @@ const CategoryBar = ({ category }) => {
     <Form.Group className="mb-3">
       <Container className="d-flex gap-2 justify-content-center mt-5 flex-wrap align-items-center">
         {category.map((item, i) => (
-          <Form.Check type="checkbox" label={item} key={i} />
+          <Form.Check type="radio" label={item} key={i} name="category"/>
         ))}
       </Container>
     </Form.Group>

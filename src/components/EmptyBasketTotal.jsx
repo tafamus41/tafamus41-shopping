9027555541aa296722cc27104356c35b4fb23024
asimov <seyranmustafa4@gmail.com>
2024-09-18 04:40:13 +0300

    p

--- src/components/EmptyBasketTotal.jsx
+++ src/components/EmptyBasketTotal.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const EmptyBasketTotal = () => {
+    const navigate=useNavigate()
   return (
-    <div className="d-flex justify-content-center align-items-center text-success">
-      <h1 className="">Your basket is already empty</h1>
+    <div className="d-flex justify-content-center align-items-center flex-column mt-5">
+      <h1 className="text-success">Your basket is already empty</h1>
+      <h1 className="text-danger">Do you wanna go back and choose something</h1>
+      <button onClick={()=>navigate("/")}>GO BACK</button>
     </div>
   );
 };
